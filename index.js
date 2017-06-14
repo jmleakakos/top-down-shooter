262fe4a2025aa9c8e5d14aba8be2c6933c5571e6
Jeremy Leakakos <nimbus57@gmail.com>
2017-06-13 18:59:32 -0600

    Adding tutorial, plus my own test.html. Test.html will have a baddie automatically walking around.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-console.log("Hello, World!")
-
 var path = require('path');
 var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
   //res.send('Hello World')
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+// /tutorial; /part1, /part2, ...
 app.get('/game/:part', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/part' + req.params['part'] + '.html'))
+  part = '/src/' + req.params['part'] + '.html';
+  res.sendFile(path.join(__dirname + part));
 })
 
-app.listen(3000)
+app.get('/game', function(req, res) {
+  part = '/src/' + 'test.html';
+  res.sendFile(path.join(__dirname + part));
+})
+
+// Used by tutorial right now
+app.get('/assets/:asset', function(req, res) {
+  res.sendFile(path.join(__dirname + '/assets/' + req.params['asset']));
+})
+
+
+  app.listen(3000)
 
